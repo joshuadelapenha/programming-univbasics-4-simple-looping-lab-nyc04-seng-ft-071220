30659167be877206404fed9d71bfc850312695dc
Joshua D <joshuadelapenha@yahoo.com>
2020-07-20 14:18:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,10 @@
 # Write your methods here
 
-def loop_message_five_time(string)
+def loop_message_five_time(message)
+  counter = 0 
+  
+  while counter < 5
+    puts message
+    counter +=1
+  end
 end
